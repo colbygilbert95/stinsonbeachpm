02c4e6adef44c6a137b58fadcd2ef61df73fa1e3
Ahmed Yasser <ahmedreo4@gmail.com>
2020-03-17 22:54:36 +0200

    m

--- src/pages/Book_Your_Rental/FilterOptions/FilterOptions.js
+++ src/pages/Book_Your_Rental/FilterOptions/FilterOptions.js
@@ -5,32 +5,26 @@ class FilterOptions extends Component {
   render() {
     return (
       <section className="book-your--top">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-9">
-              <ul className="single-post-link mt-0">
-                <li>
-                  <a href="#">Date</a>
-                </li>
-                <li>
-                  <a href="#">Guests</a>
-                </li>
-                <li>
-                  <a href="#">Price</a>
-                </li>
-                <li>
-                  <a href="#">Type of Place</a>
-                </li>
-                <li>
-                  <a href="#">More Filters</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-sm-3">
-              <div className="Show-Map-btn">
-                <span>Show Map</span>
-              </div>
-            </div>
+        <div className="flex justify-between p-30">
+          <ul className="single-post-link mt-0">
+            <li>
+              <a href="#">Date</a>
+            </li>
+            <li>
+              <a href="#">Guests</a>
+            </li>
+            <li>
+              <a href="#">Price</a>
+            </li>
+            <li>
+              <a href="#">Type of Place</a>
+            </li>
+            <li>
+              <a href="#">More Filters</a>
+            </li>
+          </ul>
+          <div className="Show-Map-btn">
+            <span>Show Map</span>
           </div>
         </div>
       </section>
