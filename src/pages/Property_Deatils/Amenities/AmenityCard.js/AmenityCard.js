d467d860095cdd9ffdd0fcbe4ca5056ba630641d
Ahmed Yasser <ahmedreo4@gmail.com>
2020-04-20 07:12:53 +0200

    task#7

--- src/pages/Property_Deatils/Amenities/AmenityCard.js/AmenityCard.js
+++ src/pages/Property_Deatils/Amenities/AmenityCard.js/AmenityCard.js
@@ -8,7 +8,7 @@ class AmentityCard extends Component {
     return (
       <div className="col-md-4 col-lg-3">
         <div className="property-services-box text-center">
-          <div className="property-services-box-inner light-bg items-center justify-center items-center flex">
+          <div className="property-services-box-inner light-bg items-center justify-center items-center flex py-20">
             {(name === 'Dryer' && url.length == 0)
               ?
               <img src="https://cdn.onlinewebfonts.com/svg/img_431641.png" width={94} height={94} className="amenities-img" />
