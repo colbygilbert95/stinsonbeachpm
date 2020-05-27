23222f9b9948958c290832032591e87940eb5fb5
Tahmid Ziko <tahmidziko@gmail.com>
2020-05-27 18:48:51 +0600

    Add Dropdown list in Home Page Search Form

--- src/pages/Form/Search_form.js
+++ src/pages/Form/Search_form.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
+import Select from "react-select";
 
 const HomeToolTip = () => {
   return (
@@ -12,6 +13,31 @@ const HomeToolTip = () => {
     </span>
   );
 };
+
+const TypeofPlaceOptions = [
+  { value: "Entire home", label: "Entire home" },
+  { value: "Private room", label: "Private room" },
+  { value: "Shared room", label: "Shared room" },
+];
+
+const NumberOfGuestOptions = [
+  { value: "1", label: "1 guest" },
+  { value: "2", label: "2 guests" },
+  { value: "3", label: "3 guests" },
+  { value: "4", label: "4 guests" },
+  { value: "5", label: "5 guests" },
+  { value: "6", label: "6 guests" },
+  { value: "7", label: "7 guests" },
+  { value: "8", label: "8 guests" },
+  { value: "9", label: "9 guests" },
+  { value: "10", label: "10 guests" },
+  { value: "11", label: "11 guests" },
+  { value: "12", label: "12 guests" },
+  { value: "13", label: "13 guests" },
+  { value: "14", label: "14 guests" },
+  { value: "15", label: "15 guests" },
+  { value: "16", label: "16 guests" },
+];
 
 export class Search_form extends Component {
   render() {
@@ -33,21 +59,19 @@ export class Search_form extends Component {
         <br />
         <br />
         <form>
-          <div className="row">
-            <div className="col-md-6 mt-15">
-              <input
-                type="text"
-                className="form-control form_text"
-                id="your_name"
-                placeholder="Type of Place"
+          <div className="row m">
+            <div className="col-md-6 col-xs-6 mt-15">
+              <Select
+                placeholder={"Type Of Place"}
+                options={TypeofPlaceOptions}
+                className="form_select"
               />
             </div>
-            <div className="col-md-6 mt-15">
-              <input
-                type="text"
-                className="form-control form_text"
-                id="your_email"
-                placeholder="Number of guests"
+            <div className="col-md-6 col-xs-6 mt-15">
+              <Select
+                placeholder={"Number of guests"}
+                options={NumberOfGuestOptions}
+                className="form_select"
               />
             </div>
           </div>
